10e50c50279d2bddc844892137533750cb23c072
Michał Spaliński <92009@student.pb.edu.pl>
2025-06-09 17:30:35 +0200

    dodano dwa rodzaje pojazdów

---
+++ src/main/java/org/example/model/Electric.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class Electric extends Car {
+    public Electric(String registration) {
+        super(registration, CarType.Electric, 3);
+    }
+}
---
+++ src/main/java/org/example/model/Motorbike.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+public class Motorbike extends Car {
+    public Motorbike(String registration) {
+        super(registration, CarType.DELIVERY, 0);
+    }
+}
